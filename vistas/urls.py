d6f22cfbc838e760f85c12e604126e31af5502e4
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-29 12:27:18 -0500

    Update urls.py

--- vistas/urls.py
+++ vistas/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.conf.urls import url, include
 
-from .views import vistaClass, vistaConf, nextClas
+from .views import vistaClass, vistaConf, nextClas, hora_list, hora_create
 from Login.views import LoginClass, l
 
 urlpatterns = [
+    
     path('',LoginClass.as_view(), name='login'),
+    path('accounts/login/',LoginClass.as_view(), name='login'),
     path('logout/',l.as_view() ,name ='logout'),
-    path('index/', vistaClass.as_view(), name='index' ),
-    path('next/',nextClas.as_view(), name='next'),
-    path('configure/',vistaConf.as_view(), name='configure' ),
+    path('index/', login_required(vistaClass.as_view()), name='index' ),
+    path('next/',login_required(nextClas.as_view()), name='next'),
+    path('configure/',login_required(vistaConf.as_view()), name='configure' ),
+    path('lista/',login_required(hora_list), name='hora_list' ),
+    url(r'^agregar',hora_create, name='hora_create'),
 ]
+ 
