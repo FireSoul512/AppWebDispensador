e2f3e1e93d46bd594c71e16290e69df23f6d40d9
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-29 12:26:56 -0500

    Update admin.py

--- vistas/admin.py
+++ vistas/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Fecha
+from .models import Fecha, Dispensar, Usuarios
 # Register your models here.
 
 admin.site.register(Fecha) 
-
+admin.site.register(Dispensar)
+admin.site.register(Usuarios)
