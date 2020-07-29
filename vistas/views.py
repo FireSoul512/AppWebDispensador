d3b1f6143c2010cecdf528041a34ba4380d878a0
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-29 14:05:53 -0500

    Update views.py

--- vistas/views.py
+++ vistas/views.py
@@ -119,6 +119,7 @@ class vistaClass(View):
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect( ('raspy2412.ddns.net',7080) )
                 mensaje = "PESO"
+                print('peso')
                 s.send(bytes(mensaje, "utf-8"))
                 msg = s.recv(1024)
                 print("El peso actual del dispensador es de: ",msg.decode("utf-8"),"g")
